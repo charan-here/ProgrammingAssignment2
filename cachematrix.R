1628e6cf13b06661d698fbef28f83dd45d93055e
charan-here <charansri343@gmail.com>
2017-02-10 00:47:13 +0530

    Final Solution with Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,16 @@
-
+# Function to make Cache Matrix
 makeCacheMatrix <- function(x = matrix()) {
 
 	m <- NULL
+	
+	# Caching th evalue of matrix
             set <- function(y) {
                     x <<- y
                     m <<- NULL
             }
             get <- function() x
+			
+			#Caching the inverse of matrix
             setinverse <- function(mean) m <<- mean
             getinverse <- function() m
             list(set = set, get = get,
@@ -16,16 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+#Function to retrieve cache matrix if already evaluated and compute and store
 
 cacheSolve <- function(x, ...) {
         
 		m <- x$getinverse()
+			#Checking existence of cahe matrix
             if(!is.null(m)) {
                     message("getting cached data")
                     return(m)
             }
             data <- x$get()
+			# Deriving inverse of matrix
             m <- solve(data, ...)
             x$setinverse(m)
             m
